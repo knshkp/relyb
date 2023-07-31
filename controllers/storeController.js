340e34a96ba6c38dcf29f79df4b95fa33eeefdb4
Kanishk Pareek <knshkpareek@gmail.com>
2023-07-31 11:00:17 +0530

    for Store

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,60 +1,58 @@
-// const Store = require("../models/storeModel");
-// const User = require("../models/userModel");
-// const cloudinary = require("cloudinary").v2; // Import Cloudinary SDK
+const Store = require("../models/storeModel");
+const User = require("../models/userModel");
+const cloudinary = require("cloudinary").v2; // Import Cloudinary SDK
 
-// cloudinary.config({
-//   cloud_name: "dyukjqemj",
-//   api_key: "975334944781146",
-//   api_secret: "USmTRR4C6ly_RDh-82Y8rhMIMzc",
-// });
+cloudinary.config({
+  cloud_name: "dyukjqemj",
+  api_key: "975334944781146",
+  api_secret: "USmTRR4C6ly_RDh-82Y8rhMIMzc",
+});
 
-// const create_store = async (req, res) => {
-//   try {
-//     const userData = await User.findOne({ _id: req.body.vendor_id });
-//     if (userData) {
-//       if (!req.body.latitude || !req.body.longitude) {
-//         res.status(200).send({ success: false, msg: "Latitude and Longitude are important." });
-//       } else {
-//         const vendorData = await Store.findOne({ vendor_id: req.body.vendor_id });
-//         if (vendorData) {
-//           res.status(200).send({ success: false, msg: "This Vendor is already created." });
-//         } else {
-//           // Upload the image to Cloudinary
-//           const cloudinaryUpload = await cloudinary.uploader.upload(req.file.path, {
-//             folder: "storeImages", // Optional: You can specify a folder in Cloudinary to organize images.
-//           });
+const create_store = async (req, res) => {
+  try {
+    const userData = await User.findOne({ _id: req.body.vendor_id });
+    if (userData) {
+      if (!req.body.latitude || !req.body.longitude) {
+        res.status(200).send({ success: false, msg: "Latitude and Longitude are important." });
+      } else {
+        const vendorData = await Store.findOne({ vendor_id: req.body.vendor_id });
+        if (vendorData) {
+          res.status(200).send({ success: false, msg: "This Vendor is already created." });
+        } else {
+          // Upload the image to Cloudinary
+          const cloudinaryUpload = await cloudinary.uploader.upload(req.file.path);
 
-//           const store = new Store({
-//             vendor_id: req.body.vendor_id,
-//             logo: cloudinaryUpload.secure_url, // Save the Cloudinary image URL
-//             name: req.body.name,
-//             phone: req.body.phone,
-//             DOB: req.body.DOB,
-//             BusinessName: req.body.BusinessName,
-//             BusinessDesc: req.body.BusinessDesc,
-//             BusinessEmail: req.body.BusinessEmail,
-//             BusinessLocation: req.body.BusinessLocation,
-//             pin: req.body.pin,
-//             ServiceLocation: {
-//               type: "Point",
-//               coordinates: [parseFloat(req.body.longitude), parseFloat(req.body.latitude)],
-//             },
-//           });
+          const store = new Store({
+            vendor_id: req.body.vendor_id,
+            logo: cloudinaryUpload.secure_url, // Save the Cloudinary image URL
+            name: req.body.name,
+            phone: req.body.phone,
+            DOB: req.body.DOB,
+            BusinessName: req.body.BusinessName,
+            BusinessDesc: req.body.BusinessDesc,
+            BusinessEmail: req.body.BusinessEmail,
+            BusinessLocation: req.body.BusinessLocation,
+            pin: req.body.pin,
+            ServiceLocation: {
+              type: "Point",
+              coordinates: [parseFloat(req.body.longitude), parseFloat(req.body.latitude)],
+            },
+          });
 
-//           const storeData = await store.save();
-//           res.status(200).send({ success: true, msg: "Store Data", data: storeData });
-//         }
-//       }
-//     } else {
-//       res.status(200).send({ success: false, msg: "Vendor Id does not exist." });
-//     }
-//   } catch (error) {
-//     res.status(400).send({ success: false, msg: "An internal server error occurred.", error: error.message });
-//   }
-// };
+          const storeData = await store.save();
+          res.status(200).send({ success: true, msg: "Store Data", data: storeData });
+        }
+      }
+    } else {
+      res.status(200).send({ success: false, msg: "Vendor Id does not exist." });
+    }
+  } catch (error) {
+    res.status(400).send({ success: false, msg: "An internal server error occurred.", error: error.message });
+  }
+};
 
-// module.exports = {
-//   create_store,
-// };
+module.exports = {
+  create_store,
+};
 
   
--- routes/storeRoutes.js
+++ routes/storeRoutes.js
@@ -1,27 +1,27 @@
-// const express = require("express");
-// const store_route = express();
-// const bodyParser = require("body-parser");
-// const multer = require("multer");
-// const path = require("path");
+const express = require("express");
+const store_route = express();
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const path = require("path");
 
-// store_route.use(bodyParser.json());
-// store_route.use(bodyParser.urlencoded({ extended: true }));
-// store_route.use(express.static('public'));
+store_route.use(bodyParser.json());
+store_route.use(bodyParser.urlencoded({ extended: true }));
+store_route.use(express.static('public'));
 
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, path.join(__dirname, '../public/storeImages'));
-//     },
-//     filename: function (req, file, cb) {
-//         const name = Date.now() + '-' + file.originalname;
-//         cb(null, name);
-//     }
-// });
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, '../public/storeImages'));
+    },
+    filename: function (req, file, cb) {
+        const name = Date.now() + '-' + file.originalname;
+        cb(null, name);
+    }
+});
 
-// const upload = multer({ storage: storage });
-// const auth = require('../middleware/auth');
-// const store_controller = require('../controllers/storeController');
+const upload = multer({ storage: storage });
+const auth = require('../middleware/auth');
+const store_controller = require('../controllers/storeController');
 
-// store_route.post('/create_partner', auth, upload.single('logo'), store_controller.create_store);
+store_route.post('/create_partner', auth, upload.single('logo'), store_controller.create_store);
 
-// module.exports = store_route;
+module.exports = store_route;

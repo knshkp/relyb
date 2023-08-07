51eff5e402c245879f236205e8f410d02d58ead7
Kanishk Pareek <knshkpareek@gmail.com>
2023-08-07 23:34:13 +0530

    conm

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser=require('body-parser');
 require('dotenv').config();
 
 const app = express();
+app.use(express.static('public'));
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
